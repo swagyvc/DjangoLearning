c1a21c7d54d2aa0b6476711f3e4528ee7ab72453
Thai Nguyen <thai.vc.nguyen@gmail.com>
2026-07-22 22:44:20 -0400

    updated repo

--- javascriptLearn/self-tought/test/script.js
+++
@@ -1,7 +0,0 @@
-
-const button = document.getElementById("myButton");
-const message = document.getElementById("messageText");
-
-button.addEventListener("click", function() {
-    message.textContent = "Hello! You clicked the button successfully.";
-});
